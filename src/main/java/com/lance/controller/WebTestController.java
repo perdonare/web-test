8d7f556b4f34ec7bdb9c073ba3c5574d32f78c82
yuanqiliang <perdonare@163.com>
2015-07-01 18:55:19 +0800

    spring-mvc

--- src/main/java/com/lance/controller/WebTestController.java
+++ src/main/java/com/lance/controller/WebTestController.java
@@ -4,7 +4,10 @@ import com.lance.persistence.mapper.UserMapper;
 import com.lance.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by perdonare on 2015/5/3.
@@ -12,9 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test")
 public class WebTestController {
+    //@RequestParam, @RequestHeader, @PathVariable
 
     @Autowired
     private UserMapper userMapper;
+
+    @RequestMapping("/form")
+    public String testPOJO(){
+        return "test/Form";
+    }
 
     @RequestMapping("/user")
     public String test(){
@@ -25,5 +34,35 @@ public class WebTestController {
         User user = userMapper.selectByPrimaryKey((long) 5);
         System.out.println(user);
         return "index";
+    }
+
+    //测试@PathVariable
+    @RequestMapping("{method}")
+    public String testPathVariable(@PathVariable String method){
+        return "test/"+"PathVariable"+method;
+    }
+
+    @RequestMapping("/pojo")
+    public ModelAndView testPOJO(User user){
+        ModelAndView modelAndView = new ModelAndView("test/User");
+        modelAndView.addObject("user",user);
+        return modelAndView;
+    }
+
+    @RequestMapping("/forward")
+    public String testForward(){
+        return "forward:/test/Add";
+    }
+
+    @RequestMapping("/redirect")
+    public String testRedirect(){
+        return "redirect:/test/Add";
+    }
+
+    @RequestMapping("/requestParam")
+    public ModelAndView testRequestParam(@RequestParam(value="_id") String id){
+        ModelAndView modelAndView = new ModelAndView("test/RequestParam");
+        modelAndView.addObject("id",id);
+        return modelAndView;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/test/PathVariableDelete.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: H7
+  Date: 2015/7/1
+  Time: 17:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+variable delete
+</body>
+</html>
