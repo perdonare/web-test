4aa572bcaa7cb95696bb4fb02de0f79ee454f256
yuanqiliang <yuanqiliang@bestpay.com.cn>
2016-03-03 10:15:12 +0800

    测试集合

---
+++ src/test/java/com/lance/test/collection/list/ListTest.java
@@ -0,0 +1,10 @@
+package com.lance.test.collection.list;
+
+/**
+ * Created by perdonare on 2016/2/24.
+ */
+public class ListTest {
+    public void testList() {
+
+    }
+}
--- src/test/java/com/lance/test/spring/TestModelNine.java
+++
@@ -1,7 +0,0 @@
-package com.lance.test.spring;
-
-/**
- * Created by perdonare on 2015/12/17.
- */
-public class TestModelNine {
-}
