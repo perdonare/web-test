158ade9cfb5ed9526f36ee0e4bc3ec60269d3cd3
袁其亮 <yuanqiliang@51kik.com>
2016-12-13 15:07:59 +0800

    jackson

--- src/test/java/com/lance/test/jackson/JacksonTest.java
+++ src/test/java/com/lance/test/jackson/JacksonTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.io.File;
